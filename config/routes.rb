b61312935eb8402b63b9246032624502b4e26cdc
Srija S <srijakumar7@gmail.com>
2019-06-14 20:29:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ root 'welcome#home'
 resources :users, only: [:new, :create]
 
 get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+post '/login' => 'sessions#create'
+post '/logout' => 'sessions#destroy'
 
 end
