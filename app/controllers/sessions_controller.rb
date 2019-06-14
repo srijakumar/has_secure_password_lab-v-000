5ed35168e68e2022913a56c3e8a69f260307460c
Srija S <srijakumar7@gmail.com>
2019-06-14 20:33:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     return redirect_to(controller: 'sessions', action: 'new') unless user
     session[:user_id] = user.id
     @user = user
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
