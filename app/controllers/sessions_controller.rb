bbbb18ec850e7d3a977655be1bf6359412cc61bc
Srija S <srijakumar7@gmail.com>
2019-06-14 20:33:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:user][:name])
     user = user.try(:authenticate, params[:user][:password])
     return redirect_to(controller: 'sessions', action: 'new') unless user
-
+    session[:user_id] = user.id
   end
 
   def destroy
