2cde71a295fe570e752e5a9c7b3da7d3d775c0d7
Srija S <srijakumar7@gmail.com>
2019-06-14 20:29:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 root 'welcome#home'
 
 resources :users, only: [:new, :create]
+
 get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
